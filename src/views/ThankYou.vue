7d3436c4a7efb6f42360bc81003027c1513245ba
yi-rui-huang <rae.yiruihuang@gmail.com>
2025-09-29 11:30:54 +0800

    prolific code

--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -18,7 +18,7 @@
         </p>
         
         <div class="verification-code">
-          <h3>Your verification code is:CRSR5GDA</h3>
+          <h3>Your verification code is:C12DOIS8</h3>
         </div>
         
         <!-- <div class="study-summary">
